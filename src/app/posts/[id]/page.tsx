7af64aa05bc0f319268a92dcd0d107c9b233e1de
Park-seungg <parkis1101@gmail.com>
2025-07-10 16:20:56 +0900

    수정7

--- src/app/posts/[id]/page.tsx
+++ src/app/posts/[id]/page.tsx
@@ -1,8 +1,26 @@
 "use client";
-import { useState } from "react";
+
+import { useParams } from "next/navigation";
+import { useEffect, useState } from "react";
 
 function PostDetail() {
-  useState;
-  return <>게시글 상세</>;
+  const params = useParams();
+  const { id } = params;
+
+  const [post, setPost] = useState({});
+  useEffect(() => {
+    fetch(`https://dummyjson.com/posts/${id}`)
+      .then((res) => res.json())
+      .then((res) => setPost(res));
+  }, []);
+
+  return (
+    <>
+      <h1>{id}번</h1>
+      <div className="text-2xl">{post.title}</div>
+      <p>{post.body}</p>
+    </>
+  );
 }
+
 export default PostDetail;
--- src/app/posts/page.tsx
+++ src/app/posts/page.tsx
@@ -1,4 +1,5 @@
 "use client";
+import Link from "next/link";
 import { useEffect, useState } from "react";
 
 function Posts() {
@@ -7,17 +8,20 @@ function Posts() {
   useEffect(() => {
     fetch("https://dummyjson.com/posts")
       .then((res) => res.json())
-      .then((res) => {
-        setPosts(res.posts);
-        console.log(res.posts);
-      });
+      .then((res) => setPosts(res.posts));
   }, []); // 생명주기에 따라 작동, 마지막 []는 의존배열 : 계속 받아오는 것을 막아준다.
 
   return (
     <ul>
       {posts.map((post) => (
-        <li>
-          {post.id} / {post.title}
+        <li key={post.id}>
+          {post.id} /
+          <Link
+            href={`/posts/${post.id}`}
+            className="p-2 rounded hover:bg-gray-100"
+          >
+            {post.title}
+          </Link>
         </li>
       ))}
     </ul>

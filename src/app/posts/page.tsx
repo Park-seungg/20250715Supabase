530fdad57d33f3a46bf5b5be51eb5445f105209a
Park-seungg <parkis1101@gmail.com>
2025-07-10 16:20:22 +0900

    수정6

--- src/app/posts/page.tsx
+++ src/app/posts/page.tsx
@@ -1,13 +1,25 @@
 "use client";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 function Posts() {
-  useState;
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    fetch("https://dummyjson.com/posts")
+      .then((res) => res.json())
+      .then((res) => {
+        setPosts(res.posts);
+        console.log(res.posts);
+      });
+  }, []); // 생명주기에 따라 작동, 마지막 []는 의존배열 : 계속 받아오는 것을 막아준다.
+
   return (
     <ul>
-      <li>게시글1</li>
-      <li>게시글2</li>
-      <li>게시글3</li>
+      {posts.map((post) => (
+        <li>
+          {post.id} / {post.title}
+        </li>
+      ))}
     </ul>
   );
 }

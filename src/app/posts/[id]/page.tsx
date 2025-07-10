df819e5fe647a7f269d062771c5a40b4b5811091
Park-seungg <parkis1101@gmail.com>
2025-07-10 16:18:25 +0900

    수정3

--- src/app/posts/[id]/page.tsx
+++ src/app/posts/[id]/page.tsx
@@ -1,4 +1,10 @@
-function PostDetail() {
-  return <>게시글 상세</>;
+function Posts() {
+  return (
+    <ul>
+      <li>게시글1</li>
+      <li>게시글2</li>
+      <li>게시글3</li>
+    </ul>
+  );
 }
-export default PostDetail;
+export default Posts;

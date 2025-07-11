294b386d867a5ada789fa2871201efec032e5fb6
Park-seungg <parkis1101@gmail.com>
2025-07-11 15:22:57 +0900

    lib,page 수정

--- src/lib/supabase.js
+++
@@ -1,6 +0,0 @@
-import { createClient } from "@supabase/supabase-js";
-
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
-const supabaseAnon = NEXT_PUBLIC_SUPABASE_ANON_KEY;
-
-createClient(supabaseUrl, supabaseAnon);

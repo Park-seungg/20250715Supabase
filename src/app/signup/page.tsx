f030615ba6a9c58c88549abe8960c9ade0a6b86b
Park-seungg <parkis1101@gmail.com>
2025-07-15 09:41:15 +0900

    useState,handleSubmit,onChange 함수 추가

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -1,9 +1,31 @@
+import { useState } from "react";
+
 function Signup() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    console.log(email, password);
+  };
+
   return (
     <>
-      <form>
-        <input type="text" name="email" placeholder="EMAIL" />
-        <input type="password" name="password" placeholder="비밀번호" />
+      <form onSubmit={handleSubmit}>
+        <input
+          type="email"
+          name="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          placeholder="EMAIL"
+        />
+        <input
+          type="password"
+          name="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          placeholder="PASSWORD"
+        />
         <button type="submit">회원가입</button>
       </form>
     </>
